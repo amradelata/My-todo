478f70feb1fa99220a9650d4eac99cef54d95a50
hesham <heshamabdulzaher@gmail.com>
2020-11-07 14:08:19 +0200

    Show loading while getting todos array

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,43 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoService } from '../todo.service'
-import { from } from 'rxjs';
-import { take, finalize } from 'rxjs/operators';
-import { Todo } from '../modelos/Todo'
-// import { indicate } from '../operators'
-import { Observable, Subject } from 'rxjs'
+import { TodoService } from '../todo.service';
+import { Todo } from '../modelos/Todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-  todos:Todo[];
+  todos: Todo[] = [];
   error = [];
-  loading = false
-  
+  loading = false;
 
-  constructor(private todoService:TodoService) { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
-    this.loading = true
-    // this.todoService.getTodos().subscribe(data => this.todos =  data,err => this.error = err);
-        this.todoService.getTodos().pipe(
-      finalize(() => this.loading = false)
-    ) .subscribe(data => this.todos =  data,err => this.error = err)
-    
-  
-    
+    this.loading = true;
+    this.todoService.getTodos().subscribe(
+      (data) => {
+        this.todos = data;
+        this.loading = false;
+      },
+      (err) => {
+        this.loading = false;
+        this.error = err;
+      }
+    );
   }
 
-    deleteTodo(todo:Todo){
-     //delete from UI
-     this.todos= this.todos.filter(t => t.id != todo.id)
-     //delete from server
-      this.todoService.deleteTodo(todo).subscribe()
-       console.log(todo)
-    }
-    addTodo(todo:Todo){
-      this.todoService.addTodo(todo).subscribe(todo => this.todos.push(todo))
-    }
-
+  deleteTodo(todo: Todo) {
+    //delete from UI
+    this.todos = this.todos.filter((t) => t.id != todo.id);
+    //delete from server
+    this.todoService.deleteTodo(todo).subscribe();
+    console.log(todo);
+  }
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo) => this.todos.push(todo));
+  }
 }
